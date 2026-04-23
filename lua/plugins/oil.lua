1f428cb6feb34026639697156ae1c380b39c3a30
rizukirr <rizkirr.xyz@gmail.com>
2026-04-23 11:06:08 +0700

    adding oil

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -2,19 +2,13 @@ return {
     "stevearc/oil.nvim",
     ---@module 'oil'
     ---@type oil.SetupOpts
-    opts = {
-        keymaps = {
-            ["sh"] = { "actions.select", opts = { horizontal = true } },
-            ["sv"] = { "actions.select", opts = { vertical = true } },
-            ["\\"] = { "actions.open_cwd", mode = "n" },
-        },
-    },
+    opts = {},
+    -- Optional dependencies
+    dependencies = { { "nvim-mini/mini.icons", opts = {} } },
     keys = {
-        -- stylua: ignore 
-        { "<leader>e", ":Oil<CR>", desc = "Open Oil" },
+        { "<leader>e", ":Oil<CR>", desc = "Oil" },
     },
-    default_file_explorer = true,
-    dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
     -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
     lazy = false,
 }
